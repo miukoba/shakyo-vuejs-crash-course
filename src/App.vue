6470a721f524f9136d59a7b4d5f0d063417ab673
Miu Kobayashi <miukoba@gmail.com>
2021-07-09 23:43:35 +0900

    add Header component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="container">
-    <h1>Hello world</h1>
+    <Header title="Task Tracker"/>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header";
+
 export default {
   name: 'App',
-  components: {}
+  components: {
+    Header
+  }
 }
 </script>
 
